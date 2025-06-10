ebf398ce8684a38b8777e4cac92d0fa0d95f7fb9
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-06-10 11:28:41 +0700

    fix:api

--- app/Http/Controllers/HargaKonsumenController.php
+++ app/Http/Controllers/HargaKonsumenController.php
@@ -7,8 +7,38 @@ use Illuminate\Http\Request;
 
 class HargaKonsumenController extends Controller
 {
-    public function index(){
-        $hargaKonsumen = HargaKonsumen::orderBy('created_at', 'desc')->get();
-        return response()->json($hargaKonsumen);
+    public function index(Request $request)
+{
+    $limit = $request->input('limit', 10);
+    $offset = $request->input('offset', 0);
+    $search = $request->input('search');
+
+    $query = HargaKonsumen::query();
+
+    if ($search) {
+        $keywords = explode(' ', $search); // pisahkan kata berdasarkan spasi
+
+        $query->where(function ($q) use ($keywords) {
+            foreach ($keywords as $word) {
+                $q->whereRaw("CONCAT_WS(' ', sales, tanggal, nama_konsumen, nama_barang, harga_jual) LIKE ?", ["%$word%"]);
+            }
+        });
     }
+
+    $total = $query->count();
+
+    $data = $query->orderBy('created_at', 'desc')
+                  ->offset($offset)
+                  ->limit($limit)
+                  ->get();
+
+    return response()->json([
+        'data' => $data,
+        'offset' => (int) $offset,
+        'limit' => (int) $limit,
+        'total' => $total,
+    ]);
+}
+
+
 }
